d7cf444ff649d743a375320f8c5b1478b764a0bb
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-09-02 21:41:05 +0000

    Added Bootstrap JS & jQuery script

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 		<!-- STYLESHEET -->
     <link rel="stylesheet" href="style.css">
+		<!-- JQUERY SCRIPT -->
+		<script src="jquery-3.6.1.min.js" defer>
+		<!-- JavaScript Bundle with Popper -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous" defer></script>
   </head>
   <body>
 		<div class="container">
@@ -17,12 +21,6 @@
     <?php 
 			echo '<p>Test Content</p>'; 
 		?> 
-
-    <!--
-    This script places a badge on your repl's full-browser view back to your repl's cover
-    page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
-    teal, blue, blurple, magenta, pink!
-    -->
     <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
   </body>
 </html>
