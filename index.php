d8447d2033b934c60416324db6e1e23d477db71c
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-09-02 21:35:54 +0000

    Integrated Bootstrap CSS (CDN)

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
-    <title>Contact Formt</title>
+		<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+		<!-- BOOTSTRAP CSS -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+		<!-- STYLESHEET -->
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <?php echo '<p>Test Content</p>'; ?> 
+		<div class="container">
+			<h1>Get in touch!</h1>
+		</div>
+    <?php 
+			echo '<p>Test Content</p>'; 
+		?> 
 
     <!--
     This script places a badge on your repl's full-browser view back to your repl's cover
