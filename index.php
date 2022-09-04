dc22c704b8a9d11fb0c9975fa59ff90409dbcb65
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-09-04 13:26:45 +0000

    Appended scripts and jQuery prevent

--- index.php
+++ index.php
@@ -10,10 +10,12 @@
 		<!-- STYLESHEET -->
     <link rel="stylesheet" href="style.css">
 		<!-- JQUERY SCRIPT -->
-		<script src="jquery-3.6.1.min.js" defer>
+		<script src="jquery-3.6.1.min.js" defer></script>
 		<!-- JavaScript Bundle with Popper -->
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous" defer></script>
-  </head>
+  	<!-- MAIN SCRIPT -->
+		<script src="script.js" defer></script>
+	</head>
   <body>
 		<div class="container">
 			<h1>Get in touch!</h1>
@@ -32,7 +34,6 @@
 				  <textarea class="form-control" placeholder="Leave a comment here" id="message" style="height: 100px"></textarea>
 				  <label for="content">Please enter your comment</label>
 				</div>
-
 			  <button type="submit" class="btn btn-primary">Submit</button>
 		</form>
 		</div>
